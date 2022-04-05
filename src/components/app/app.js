2812df013621f1d07f01dfbdded27c67cae90528
Algerdi <kzlv.vldslv@gmail.com>
2022-04-05 18:40:10 +0300

    working project

---
+++ src/components/app/app.js
@@ -0,0 +1,89 @@
+import React, {Component} from 'react';
+import {Col, Row, Container} from 'reactstrap';
+import Header from '../header';
+import RandomChar from '../randomChar';
+import ErrorMessage from '../errorMessage';
+import CharacterPage from '../characterPage';
+import ItemList from '../itemList';
+import CharDetails from '../charDetails';
+import GotService from '../../services/gotService';
+
+
+
+export default class App extends Component {
+
+    gotService = new GotService();
+
+    state = {
+        showRandomChar: true,
+        error: false,
+    }
+    // toggleRandomChar = () => {
+    //     this.setState((state) => {
+    //         return {
+    //             showRandomChar: !state.showRandomChar
+    //         }
+    //     });
+    // }
+
+    componentDidCatch() {
+        this.setState({
+            error: true
+        })
+    }
+
+    toggleRandomChar = () => {
+        this.setState({
+            showRandomChar: !this.state.showRandomChar
+        })
+    }
+
+    render() {
+        if (this.state.error) {
+            return <ErrorMessage/>
+        }
+
+        const char = this.state.showRandomChar ? <RandomChar/> : null;
+
+        return (
+            <> 
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                            {char}
+                            <button 
+                                className="toggle-btn"
+                                onClick={this.toggleRandomChar}>Toggle random character</button>
+                        </Col>
+                    </Row>
+                    <CharacterPage />
+                    <Row>
+                        <Col md='6'>
+                            <ItemList 
+                            onItemSelected={this.onItemSelected}
+                            getData={this.gotService.getAllBooks}
+                            renderItem={(item) => item.name}/>
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails charId={this.state.selectedChar}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList 
+                            onItemSelected={this.onItemSelected}
+                            getData={this.gotService.getAllHouses}
+                            renderItem={(item) => item.name}/>
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails charId={this.state.selectedChar}/>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
+};
---
+++ src/services/gotService.js
@@ -0,0 +1,90 @@
+export default class GotService {
+    constructor() {
+        this._apiBase = 'https://www.anapioficeandfire.com/api';
+    }
+
+    getResource = async (url) => {
+        const res = await fetch(`${this._apiBase}${url}`);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, recived ${res.status}`);
+        }
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const result = await this.getResource('/characters?page=5&pageSize=10');
+        return result.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const character = await this.getResource(`/characters/${id}`);
+        return this._transformCharacter(character);
+    }
+
+    getAllHouses = async () => {
+        const result = await this.getResource(`/houses/`);
+        return result.map(this._transformHouse)
+    }
+
+    getHouse = async (id) => {
+        const house = await this.getResource(`/houses/${id}`);
+        return this._transformHouse(house);
+    }
+
+    getAllBooks = async () => {
+        const result = await this.getResource(`/books/`);
+        return result.map(this._transformBook);
+    }
+
+    getBook = async (id) => {
+        const book = await this.getResource(`/books/${id}`);
+        return this._transformBook(book);
+    }
+
+    isSet(data) {
+        console.log(data)
+        if (data) {
+            return data
+        } else {
+            return 'no data :('
+        }
+    }
+
+    _extractId = (item) => {
+        const idRegExp = /\/([0-9]*)$/;
+        return item.url.match(idRegExp)[1];
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            id: this._extractId(char),
+            name: char.name,
+            gender: char.gender,
+            born: char.born,
+            died: char.died,
+            culture: char.culture,
+        }
+    }
+
+    _transformHouse = (house) => {
+        return {
+            id: this._extractId(house),
+            name: house.name,
+            region: house.region,
+            words: house.words,
+            titles: house.titles,
+            overlord:house.overlord,
+            ancestralWeapons: house.ancestralWeapons,
+        }
+    }
+
+    _transformBook = (book) => {
+        return {
+            id: this._extractId(book),
+            name: book.name,
+            numberOfPages: book.numberOfPages,
+            publisher: book.publisher,
+            released: book.released,
+        }
+    }
+}
